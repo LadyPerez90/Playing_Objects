e340831b3c5bf2aece64f844f41f53faabb6ecb8
LadyPerez90 <josefa_groce@yahoo.com>
2021-03-17 16:36:00 -0500

    first commit

---
+++ Playing_Objects.js
@@ -0,0 +1,12 @@
+var users = [{name: "Michael", age: 37}, {name: "John", age: 30}, {name: "David", age: 27}];
+
+//Print/log John's age
+console.log(users[1].age);
+
+//Print name of first object
+console.log(user[0].name);
+
+//Print the name and age of each user using a loop
+for (var i=0; i<user.length; i++){
+console.log(user.name, "-", user.age);
+}
